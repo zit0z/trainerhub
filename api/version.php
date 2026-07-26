d820d2b0853639b6ee5053a25e155e778d431bd2
SweetCheat CI <sweetcheat@sayfespace.online>
2026-07-26 20:47:34 +0000

    SweetCheat Engine v0.9.6: fix version.txt, download links point to SweetCheat-Setup.exe

--- api/version.php
+++ api/version.php
@@ -2,8 +2,8 @@
 header('Content-Type: application/json');
 echo json_encode([
     'success' => true,
-    'version' => '0.9.5',
+    'version' => '0.9.6',
     'brand' => 'SweetCheat Engine',
-    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe',
-    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe'
+    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.6',
+    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.6'
 ]);
