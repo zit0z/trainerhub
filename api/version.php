567c6e29acf318a15fc8b7bf3d1ae5ed7a8fd357
SweetCheat CI <sweetcheat@sayfespace.online>
2026-07-27 17:42:51 +0000

    SweetCheat Engine v0.9.7: 2027 ready landingpage, animated counters, scroll-reveal, polished admin

--- api/version.php
+++ api/version.php
@@ -2,8 +2,8 @@
 header('Content-Type: application/json');
 echo json_encode([
     'success' => true,
-    'version' => '0.9.6',
+    'version' => '0.9.7',
     'brand' => 'SweetCheat Engine',
-    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.6',
-    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.6'
+    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.7',
+    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.7'
 ]);
