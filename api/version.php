69114376970feba5b5ab87f7856f1cb8e510720d
SweetCheat CI <sweetcheat@sayfespace.online>
2026-07-28 20:35:31 +0000

    SweetCheat Engine v0.9.13: Total rewrite of Login/Register pages (2027 Look)

--- api/version.php
+++ api/version.php
@@ -2,8 +2,8 @@
 header('Content-Type: application/json');
 echo json_encode([
     'success' => true,
-    'version' => '0.9.10',
+    'version' => '0.9.13',
     'brand' => 'SweetCheat Engine',
-    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.10',
-    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.10'
+    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.13',
+    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.13'
 ]);
