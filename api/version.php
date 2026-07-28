a2376350e58acefc44f7ff6f8d2f607374587828
SweetCheat CI <sweetcheat@sayfespace.online>
2026-07-28 18:57:58 +0000

    SweetCheat Engine v0.9.9: Admin File Manager fixed, DB filled, 2027 ready landingpage final polish

--- api/version.php
+++ api/version.php
@@ -1,9 +1,9 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: own/json');
 echo json_encode([
     'success' => true,
-    'version' => '0.9.8',
+    'version' => '0.9.9',
     'brand' => 'SweetCheat Engine',
-    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.8',
-    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.8'
+    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.9',
+    'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-Setup.exe?v=0.9.9'
 ]);
