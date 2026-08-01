3a338d08f02abcc6f03727755e2b77187666e408
SweetCheat CI <sweetcheat@sayfespace.online>
2026-08-01 09:35:45 +0000

    Slayer Nuclear Build Trigger

--- api/version.php
+++ api/version.php
@@ -2,8 +2,8 @@
 header('Content-Type: application/json');
 echo json_encode([
     'success' => true,
-    'version' => '0.9.13',
+    'version' => '0.9.14',
     'brand' => 'SweetCheat Engine',
-    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-windows.zip?v=0.9.13',
+    'download_url' => 'https://sayfespace.online/trainerhub/SweetCheat-windows.zip?v=0.9.14',
     'installer_url' => 'https://sayfespace.online/trainerhub/SweetCheat-v0914.exe'
 ]);
